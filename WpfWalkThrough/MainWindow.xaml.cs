e61f5a9609154e9b7cf1ad098b04335717551984
Pavel Svec <pavel.svec@itcrawl.cz>
2024-02-16 13:45:41 +0100

    Add project files.

---
+++ WpfWalkThrough/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWalkThrough
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //MessageBox.Show($"Mouse Up on element {e.Source}");
+            label.Content += "\n" +e.Source.ToString();
+        }
+    }
+}
