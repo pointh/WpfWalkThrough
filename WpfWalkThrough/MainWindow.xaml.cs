ddd516267938d190f913937062267574e253dd2c
Pavel Svec <pavel.svec@itcrawl.cz>
2024-03-18 10:47:36 +0100

    Zapojení ObservableCollection

--- WpfWalkThrough/MainWindow.xaml.cs
+++ WpfWalkThrough/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
 
@@ -11,13 +12,12 @@ namespace WpfWalkThrough
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public List<string> History {  get; set; }
+        // **ObservableCollection** má schopnost volat aktualizaci připojeného ListView
+        // při kažné změně obsahu
+        public ObservableCollection<string> History {  get; set; }
 
-        // Tohle je "plain vanilla" řešení pro binding
-        // privátní pole třídy typu string ...
         string? lastClick;
 
-        // ... je odkazované z vlastnosti podobného jména - Konvence: vlastnosti jsou s velkým počátečním písmenem
         public string? LastClick 
         { 
             get => lastClick; 
@@ -29,28 +29,9 @@ namespace WpfWalkThrough
                 if (value != null) 
                 {
                     History.Add(value);
-
-                    // Tohle je hack, který aktualizuje zobrazení ListView souběžně s History
-                    // Lepší je použít typ ObservableCollection<string> ...
-                    LVHistory.ItemsSource = null;
-                    LVHistory.ItemsSource = History;
-                    // Ani následující nepomůže:
-                    // PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(History)));
-                    //
-                    // Je to chyba ListView nebo vlastnost, která nám umožňuje rozhodovat o tom, kdy dojde 
-                    // k překreslení? 
-                    // Může se to hodit, když např. načítáme History ze souboru a přidáváme řádek po řádku.
-                    // Kdyby se ListView překreslovalo při každém přidání řádku, program by to hodně zpomalilo.
                 }
                 
-                // ... oznam WPF UI, že se musí překreslit část uživatelského interface,
-                // která má binding na LastClick.
-                // Operátor ?. zajistí, že se Invoke zavolá jenom když PropertyChanged != null
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastClick)));
-                // To je stejné jako PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastClick")); 
-                // nameof(LastClick) nám kompilátor vyhodí jako chybu, když změníme název vlastnosti LastClick,
-                // což u stringu "LastClick" udělat nemůže - string jako string
-                // To může vést k nepříjemným chybám při REFAKTORINGU
             }
         }
 
@@ -58,22 +39,13 @@ namespace WpfWalkThrough
         {
             InitializeComponent();
 
-            // všechny valastnosti, které mají binding z příslušného XAML souboru (MainWindow.xaml)
-            // se budou hledat v této třídě.
-            History = new List<string>();
+            History = new ObservableCollection<string>();
             DataContext = this;
         }
 
         private void Window_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
-            // Tady jenom měníme hodnotu vlastnosti,
-            // takže se zavolá její setter
-            // a ze setteru se zavolá PropertyChanged.Invoke(...)
             LastClick = e?.Source?.ToString()?.ToUpper();
-            // V tomto případě se tedy vyhneme přímému volání
-            // jména prvku UI, který se má změnit.
-            // To nám umožňuje zpracovávat a testova logiku aplikace bez ohledu 
-            // na její prezentaci v GUI, kterou můžeme realizovat bindingem později.
         }
     }
 }
