8cedb89b88d1be5ea2519f05427c87fa9453bbf8
Pavel Svec <pavel.svec@itcrawl.cz>
2024-03-18 10:41:51 +0100

    ListView  a List jako ItemsSource

--- WpfWalkThrough/MainWindow.xaml.cs
+++ WpfWalkThrough/MainWindow.xaml.cs
@@ -1,15 +1,6 @@
 ﻿using System.ComponentModel;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using WpfWalkThrough.Properties;
 
 namespace WpfWalkThrough
 {
@@ -18,19 +9,40 @@ namespace WpfWalkThrough
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public List<string> History {  get; set; }
+
         // Tohle je "plain vanilla" řešení pro binding
         // privátní pole třídy typu string ...
-        string lastClick;
+        string? lastClick;
 
         // ... je odkazované z vlastnosti podobného jména - Konvence: vlastnosti jsou s velkým počátečním písmenem
-        public string LastClick 
+        public string? LastClick 
         { 
             get => lastClick; 
             set 
             { 
                 // Když se změní hodnota vlastnosti ...
-                lastClick = value; 
+                lastClick = value;
+                
+                if (value != null) 
+                {
+                    History.Add(value);
 
+                    // Tohle je hack, který aktualizuje zobrazení ListView souběžně s History
+                    // Lepší je použít typ ObservableCollection<string> ...
+                    LVHistory.ItemsSource = null;
+                    LVHistory.ItemsSource = History;
+                    // Ani následující nepomůže:
+                    // PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(History)));
+                    //
+                    // Je to chyba ListView nebo vlastnost, která nám umožňuje rozhodovat o tom, kdy dojde 
+                    // k překreslení? 
+                    // Může se to hodit, když např. načítáme History ze souboru a přidáváme řádek po řádku.
+                    // Kdyby se ListView překreslovalo při každém přidání řádku, program by to hodně zpomalilo.
+                }
+                
                 // ... oznam WPF UI, že se musí překreslit část uživatelského interface,
                 // která má binding na LastClick.
                 // Operátor ?. zajistí, že se Invoke zavolá jenom když PropertyChanged != null
@@ -48,29 +60,20 @@ namespace WpfWalkThrough
 
             // všechny valastnosti, které mají binding z příslušného XAML souboru (MainWindow.xaml)
             // se budou hledat v této třídě.
+            History = new List<string>();
             DataContext = this;
         }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
 
         private void Window_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
             // Tady jenom měníme hodnotu vlastnosti,
             // takže se zavolá její setter
             // a ze setteru se zavolá PropertyChanged.Invoke(...)
-            LastClick = e.Source.ToString().ToUpper();
+            LastClick = e?.Source?.ToString()?.ToUpper();
             // V tomto případě se tedy vyhneme přímému volání
             // jména prvku UI, který se má změnit.
             // To nám umožňuje zpracovávat a testova logiku aplikace bez ohledu 
             // na její prezentaci v GUI, kterou můžeme realizovat bindingem později.
-
-            // Tady přímo měníme obsah prvku, který voláme přímo jménem (label1)
-            label1.Content += "\n" +e.Source.ToString();
-        }
-
-        private void ButtonImage_MouseEnter(object sender, MouseEventArgs e)
-        {
-            label1.Content += "\n" + "Myš přišla";
         }
     }
 }
